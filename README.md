c8629ac7902a293705725706670eaaaf5159ba89
Michael Byrne <mpbyrne@gmail.com>
2016-09-28 23:59:31 -0700

    working

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# assignment1-ang
+# coursera
---
+++ app.js
@@ -0,0 +1,32 @@
+/**
+ * Created by michaelbyrne on 9/28/16.
+ */
+
+(function () {
+    'use strict';
+    angular.module('LunchChecker',[]).controller('CheckerController',CheckerController);
+    CheckerController.$inject = ['$scope'];
+    function CheckerController($scope) {
+        $scope.list = "";
+        $scope.count = 0;
+        $scope.message = "";
+        $scope.countItems = function () {
+            //var listArr = $scope.list.split(",");
+            var reSpaces = /(,\s*,)+/g;
+            var reTrail =  /,\s*$/;
+            $scope.list = $scope.list.replace(reSpaces,",");
+            var listArr = $scope.list.replace(reTrail,"").split(",");
+            $scope.count = listArr.length;
+        };
+        $scope.checkCount = function() {
+            if($scope.count <= 3){
+                $scope.message = "enjoy!";
+            }
+            else{
+                $scope.message = "too much!";
+            }
+        }
+    }
+
+
+})();
